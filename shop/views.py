356809ea06ce3596a845dbdc632e5c68b4545d7e
Kunal Goyal <80157692+Kunal1701@users.noreply.github.com>
2023-08-15 18:50:36 +0530

    Ecommerce using Django and Javascript

---
+++ shop/views.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from .models import *
+from django.core.paginator import Paginator
+
+# Create your views here.
+
+
+def index(request):
+    product_objects = Products.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        product_objects = product_objects.filter(title__icontains=item_name)
+
+    paginator = Paginator(product_objects, 4)
+    page = request.GET.get('page', None)
+    if page == None or page == "":
+        page = 1
+    product_objects = paginator.page(page)
+    return render(request, 'shop/index.html', {'product_objects': product_objects})
+
+
+def detail(request, x):
+    product_object = Products.objects.get(id=x)
+    return render(request, 'shop/detail.html', {'product_object': product_object})
+
+
+def checkout(request):
+    if request.method == "POST":
+        items = request.POST.get('items', '')
+        name = request.POST.get('name', "")
+        email = request.POST.get('email', "")
+        address = request.POST.get('address', "")
+        city = request.POST.get('city', "")
+        state = request.POST.get('state', "")
+        zipcode = request.POST.get('zipcode', "")
+        total = request.POST.get('total', "")
+
+        order = Order(items=items,name=name,email=email,address=address,city=city,state=state,zipcode=zipcode,total=total)
+        order.save()
+    return render(request, 'shop/checkout.html')
